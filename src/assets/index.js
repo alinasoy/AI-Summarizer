d1550d63d4fc232aba809d5963ad21f23b0b7e5b
alinasoy <alinasoy0501@gmail.com>
2023-08-07 13:02:34 +0530

    Initial message

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import tick from './tick.svg'
+import logo from './logo.png'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    tick,
+    logo
+}
